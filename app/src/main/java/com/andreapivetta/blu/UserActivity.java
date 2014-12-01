36097601e36c271a5b4831dbacb1bf7582a2dc8b
Andrea Pivetta <vanpivix@gmail.com>
2014-12-01 02:02:03 +0100

    basic follow/unfollow capabilities

--- app/src/main/java/com/andreapivetta/blu/UserActivity.java
+++ app/src/main/java/com/andreapivetta/blu/UserActivity.java
@@ -1,22 +1,33 @@
 package com.andreapivetta.blu;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.style.StyleSpan;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.andreapivetta.blu.twitter.FollowTwitterUser;
 import com.andreapivetta.blu.twitter.TwitterUtils;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
+import twitter4j.Relationship;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.User;
@@ -24,14 +35,17 @@ import twitter4j.User;
 
 public class UserActivity extends ActionBarActivity {
 
+    private final static int I_FOLLOW_HIM = 0;
+    private final static int WE_FOLLOW_EACH_OTHER = 2;
+    private final static int I_DONT_KNOW_WHO_YOU_ARE = -1;
+    private int STATUS;
+
     private Twitter twitter;
     private User user;
-
     private Toolbar toolbar;
     private ImageView profileBackgroundImageView, profilePictureImageView;
-    private TextView userNickTextView, userNameTextView, descriptionTextView, userLocationTextView, userWebsiteTextView;
-
-    private TextView tweetAmountTextView, followingAmountTextView, followersAmountTextView;
+    private TextView userNickTextView, userNameTextView, descriptionTextView, userLocationTextView, userWebsiteTextView, tweetAmountTextView, followingAmountTextView, followersAmountTextView;
+    private ImageButton followImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,13 +79,28 @@ public class UserActivity extends ActionBarActivity {
         tweetAmountTextView = (TextView) findViewById(R.id.tweetAmountTextView);
         followingAmountTextView = (TextView) findViewById(R.id.followingAmountTextView);
         followersAmountTextView = (TextView) findViewById(R.id.followersAmountTextView);
+        followImageButton = (ImageButton) findViewById(R.id.followImageButton);
     }
 
     void setUpUI() {
         Picasso.with(UserActivity.this)
                 .load(user.getProfileBannerURL())
                 .placeholder(getResources().getDrawable(R.drawable.placeholder_banner))
-                .into(profileBackgroundImageView);
+                .into(profileBackgroundImageView, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        Bitmap onePixelBitmap = Bitmap.createScaledBitmap(((BitmapDrawable) profileBackgroundImageView.getDrawable()).getBitmap(), 1, 1, true);
+                        int pixel = onePixelBitmap.getPixel(0, 0);
+
+                        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+                            getWindow().setStatusBarColor(Color.rgb(Color.red(pixel), Color.green(pixel), Color.blue(pixel)));
+                    }
+
+                    @Override
+                    public void onError() {
+
+                    }
+                });
 
         Picasso.with(UserActivity.this)
                 .load(user.getOriginalProfileImageURL())
@@ -80,8 +109,72 @@ public class UserActivity extends ActionBarActivity {
         userNameTextView.setText(user.getName());
         userNickTextView.setText("@" + user.getScreenName());
         descriptionTextView.setText(user.getDescription());
-        userLocationTextView.setText(user.getLocation());
-        userWebsiteTextView.setText(user.getURLEntity().getDisplayURL());
+
+        String location = user.getLocation();
+        if (location.length() != 0)
+            userLocationTextView.setText(location);
+        else
+            userLocationTextView.setVisibility(View.GONE);
+
+        String website = user.getURLEntity().getDisplayURL();
+        if (website.length() != 0)
+            userWebsiteTextView.setText(website);
+        else
+            userWebsiteTextView.setVisibility(View.GONE);
+
+        switch (STATUS) {
+            case I_FOLLOW_HIM:
+                followImageButton.setBackground(getResources().getDrawable(R.drawable.circle_button_blue));
+                followImageButton.setImageResource(R.drawable.ic_person_add_white_24dp);
+                break;
+            case WE_FOLLOW_EACH_OTHER:
+                followImageButton.setBackground(getResources().getDrawable(R.drawable.circle_button_green));
+                followImageButton.setImageResource(R.drawable.ic_person_add_white_24dp);
+                break;
+            default:
+                break;
+        }
+
+        followImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(UserActivity.this);
+
+                switch (STATUS) {
+                    case I_FOLLOW_HIM:
+                        builder.setTitle(getString(R.string.you_are_following))
+                                .setMessage(getString(R.string.stop_following) + " " + user.getName() + "?")
+                                .setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        new FollowTwitterUser(UserActivity.this, twitter, false).execute(user.getId());
+                                    }
+                                });
+                        break;
+                    case WE_FOLLOW_EACH_OTHER:
+                        builder.setTitle(getString(R.string.you_and) + " " + user.getName() + " " + getString(R.string.are_following_ea))
+                                .setMessage(getString(R.string.stop_following) + " " + user.getName() + "?")
+                                .setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        new FollowTwitterUser(UserActivity.this, twitter, false).execute(user.getId());
+                                    }
+                                });
+                        break;
+                    default:
+                        builder.setTitle(getString(R.string.follow) + " " +  user.getName() + "?")
+                                .setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        new FollowTwitterUser(UserActivity.this, twitter, true).execute(user.getId());
+                                    }
+                                });
+                        break;
+                }
+
+                builder.setNegativeButton(getString(R.string.cancel), null).create().show();
+            }
+        });
 
         String amount = user.getStatusesCount() + "";
         StyleSpan b = new StyleSpan(android.graphics.Typeface.BOLD);
@@ -101,6 +194,20 @@ public class UserActivity extends ActionBarActivity {
         followersAmountTextView.setText(sb);
 
         tweetAmountTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        followingAmountTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        followersAmountTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -131,6 +238,16 @@ public class UserActivity extends ActionBarActivity {
         protected Boolean doInBackground(Long... params) {
             try {
                 user = twitter.showUser(params[0]);
+
+                Relationship rel = twitter.showFriendship(twitter.getId(), user.getId());
+                if(rel.isSourceFollowingTarget() && rel.isTargetFollowingSource()) {
+                    STATUS = WE_FOLLOW_EACH_OTHER;
+                } else if (rel.isSourceFollowingTarget()) {
+                    STATUS = I_FOLLOW_HIM;
+                } else {
+                    STATUS = I_DONT_KNOW_WHO_YOU_ARE;
+                }
+
             } catch (TwitterException e) {
                 e.printStackTrace();
                 return false;
---
+++ app/src/main/java/com/andreapivetta/blu/twitter/FollowTwitterUser.java
@@ -0,0 +1,45 @@
+package com.andreapivetta.blu.twitter;
+
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+
+public class FollowTwitterUser  extends AsyncTask<Long, Void, Boolean> {
+
+    private Context context;
+    private Twitter twitter;
+    private boolean follow;
+
+    public FollowTwitterUser(Context context, Twitter twitter, boolean follow) {
+        this.context = context;
+        this.twitter = twitter;
+        this.follow = follow;
+    }
+
+    protected Boolean doInBackground(Long... args) {
+        try {
+            if (follow)
+                twitter.createFriendship(args[0]);
+            else
+                twitter.destroyFriendship(args[0]);
+        } catch (TwitterException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    protected void onPostExecute(Boolean status) {
+        if (status) {
+            Toast.makeText(context, "You're now following this user", Toast.LENGTH_SHORT).show();
+        } else {
+            Log.i("FOLLOW STATUS", "Action not performed");
+        }
+    }
+}
