f75c0d8e0420f9b2d7c8bcf629ffb1857beca2c3
Andrea Pivetta <vanpivix@gmail.com>
2015-06-23 01:28:39 +0200

    white tab indicator

--- app/src/main/java/com/andreapivetta/blu/activities/TimeLineActivity.java
+++ app/src/main/java/com/andreapivetta/blu/activities/TimeLineActivity.java
@@ -11,7 +11,6 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
