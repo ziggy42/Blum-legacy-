75cc7fac243790d96471f30afd09b01693b49266
Andrea Pivetta <vanpivix@gmail.com>
2015-06-26 20:22:10 +0200

    profiles in about are now opened in Blum

--- app/src/main/java/com/andreapivetta/blu/activities/ConversationActivity.java
+++ app/src/main/java/com/andreapivetta/blu/activities/ConversationActivity.java
@@ -11,7 +11,6 @@ import android.preference.PreferenceManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
