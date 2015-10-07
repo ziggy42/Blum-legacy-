985ef6b34da9d7c90fbc53c343838076d03249b1
ziggy42 <vanpivix@gmail.com>
2015-10-07 21:22:08 +0200

    [APK 39]

--- app/src/main/java/com/andreapivetta/blu/receivers/BootReceiver.java
+++ app/src/main/java/com/andreapivetta/blu/receivers/BootReceiver.java
@@ -3,7 +3,6 @@ package com.andreapivetta.blu.receivers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
 import com.andreapivetta.blu.R;
@@ -15,8 +14,7 @@ public class BootReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        SharedPreferences mPref = PreferenceManager.getDefaultSharedPreferences(context);
-        if (mPref.getBoolean(context.getString(R.string.pref_key_stream_service), false))
+        if (PreferenceManager.getDefaultSharedPreferences(context).getBoolean(context.getString(R.string.pref_key_stream_service), false))
             context.startService(new Intent(context, StreamNotificationService.class));
         else
             BasicNotificationService.startService(context);
