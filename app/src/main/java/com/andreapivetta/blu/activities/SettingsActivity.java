4be2b0a06131846afe8bf6526f61340955dcf227
ziggy42 <vanpivix@gmail.com>
2015-11-22 01:53:56 +0100

    Remove Ads directly from HomeActivity

--- app/src/main/java/com/andreapivetta/blu/activities/SettingsActivity.java
+++ app/src/main/java/com/andreapivetta/blu/activities/SettingsActivity.java
@@ -18,8 +18,6 @@ import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.webkit.WebView;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,19 +27,9 @@ import com.andreapivetta.blu.services.BasicNotificationService;
 import com.andreapivetta.blu.services.CheckFollowingService;
 import com.andreapivetta.blu.services.StreamNotificationService;
 import com.andreapivetta.blu.twitter.TwitterUtils;
-import com.andreapivetta.blu.twitter.UpdateTwitterStatus;
-import com.anjlab.android.iab.v3.BillingProcessor;
-import com.anjlab.android.iab.v3.SkuDetails;
-import com.anjlab.android.iab.v3.TransactionDetails;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 
-public class SettingsActivity extends ThemedActivity implements BillingProcessor.IBillingHandler {
-
-    private BillingProcessor billingProcessor;
+public class SettingsActivity extends ThemedActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,51 +39,10 @@ public class SettingsActivity extends ThemedActivity implements BillingProcessor
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        billingProcessor = new BillingProcessor(this, null, this);
-
         if (savedInstanceState == null)
             getFragmentManager().beginTransaction()
                     .add(R.id.container, new SettingsFragment())
                     .commit();
-    }
-
-    @Override
-    public void onProductPurchased(String s, TransactionDetails transactionDetails) {
-        PreferenceManager.getDefaultSharedPreferences(this).edit().putBoolean(getString(R.string.pref_ads_removed), true).commit();
-        Toast.makeText(this, R.string.completed_purchase, Toast.LENGTH_SHORT).show();
-        finish();
-        final Intent intent = new Intent(this, HomeActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | IntentCompat.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
-    }
-
-    @Override
-    public void onPurchaseHistoryRestored() {
-
-    }
-
-    @Override
-    public void onBillingError(int i, Throwable throwable) {
-        Toast.makeText(this, R.string.failed_purchase, Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onBillingInitialized() {
-
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (!billingProcessor.handleActivityResult(requestCode, resultCode, data))
-            super.onActivityResult(requestCode, resultCode, data);
-    }
-
-    @Override
-    public void onDestroy() {
-        if (billingProcessor != null)
-            billingProcessor.release();
-
-        super.onDestroy();
     }
 
     public static class SettingsFragment extends PreferenceFragment {
@@ -175,78 +122,6 @@ public class SettingsActivity extends ThemedActivity implements BillingProcessor
                     return true;
                 }
             });
-
-            final Preference adsPreference = findPreference("pref_key_iap");
-            if (mSharedPreferences.getBoolean(getString(R.string.pref_ads_removed), false)) {
-                adsPreference.setEnabled(false);
-            } else {
-                adsPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-                    @Override
-                    public boolean onPreferenceClick(Preference preference) {
-                        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-                        View dialogView = View.inflate(getActivity(), R.layout.dialog_iap, null);
-
-                        String[] ids = new String[]{"remove_ads_099", "remove_ads_199", "remove_ads_049", "remove_ads_999"};
-                        List<SkuDetails> details = ((SettingsActivity) getActivity()).billingProcessor.getPurchaseListingDetails(new ArrayList<>(Arrays.asList(ids)));
-                        for (SkuDetails detail : details) {
-                            if (ids[0].equals(detail.productId)) {
-                                ((RadioButton) dialogView.findViewById(R.id.iap_099_radioButton)).setText(detail.priceText);
-                            } else if (ids[1].equals(detail.productId)) {
-                                ((RadioButton) dialogView.findViewById(R.id.iap_199_radioButton)).setText(detail.priceText);
-                            } else if (ids[2].equals(detail.productId)) {
-                                ((RadioButton) dialogView.findViewById(R.id.iap_499_radioButton)).setText(detail.priceText);
-                            } else {
-                                ((RadioButton) dialogView.findViewById(R.id.iap_999_radioButton)).setText(detail.priceText);
-                            }
-                        }
-
-                        ((RadioButton) dialogView.findViewById(R.id.tweet_radioButton)).setText(getString(R.string.tweet));
-
-                        RadioGroup radioGroup = (RadioGroup) dialogView.findViewById(R.id.iapRadioGroup);
-                        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-                            @Override
-                            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                                switch (checkedId) {
-                                    case R.id.iap_099_radioButton:
-                                        ((SettingsActivity) getActivity()).billingProcessor.purchase(getActivity(), "remove_ads_099");
-                                        break;
-                                    case R.id.iap_199_radioButton:
-                                        ((SettingsActivity) getActivity()).billingProcessor.purchase(getActivity(), "remove_ads_199");
-                                        break;
-                                    case R.id.iap_499_radioButton:
-                                        ((SettingsActivity) getActivity()).billingProcessor.purchase(getActivity(), "remove_ads_049");
-                                        break;
-                                    case R.id.iap_999_radioButton:
-                                        ((SettingsActivity) getActivity()).billingProcessor.purchase(getActivity(), "remove_ads_999");
-                                        break;
-                                    case R.id.tweet_radioButton:
-                                        new AlertDialog.Builder(getActivity()).setTitle(getString(R.string.pay_with_tweet_title))
-                                                .setMessage(getString(R.string.pay_with_tweet_status))
-                                                .setPositiveButton(getString(R.string.tweet), new DialogInterface.OnClickListener() {
-                                                    @SuppressLint("CommitPrefEdits")
-                                                    @Override
-                                                    public void onClick(DialogInterface dialog, int which) {
-                                                        new UpdateTwitterStatus(getActivity(), TwitterUtils.getTwitter(getActivity()), -1l)
-                                                                .execute(getString(R.string.pay_with_tweet_status));
-                                                        mSharedPreferences.edit().putBoolean(getString(R.string.pref_ads_removed), true).commit();
-                                                        Toast.makeText(getActivity(), R.string.completed_purchase, Toast.LENGTH_SHORT).show();
-                                                        restartApplication();
-                                                    }
-                                                }).setNegativeButton(getString(R.string.keep_ads), null)
-                                                .create().show();
-                                        break;
-                                }
-                            }
-                        });
-
-                        builder.setTitle(getString(R.string.iap_pref_title))
-                                .setView(dialogView)
-                                .create().show();
-
-                        return true;
-                    }
-                });
-            }
 
             findPreference("pref_key_edit").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                 @Override
