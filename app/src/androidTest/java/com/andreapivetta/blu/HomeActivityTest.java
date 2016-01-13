e4e43254630059afcfc18cdfc982a45b481ae897
ziggy42 <vanpivix@gmail.com>
2016-01-13 23:08:21 +0100

    added androidTest because why not

---
+++ app/src/androidTest/java/com/andreapivetta/blu/HomeActivityTest.java
@@ -0,0 +1,43 @@
+package com.andreapivetta.blu;
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.Toolbar;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.widget.TextView;
+
+import com.andreapivetta.blu.activities.HomeActivity;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class HomeActivityTest {
+
+    @Rule
+    public ActivityTestRule<HomeActivity> mActivityRule = new ActivityTestRule<>(
+            HomeActivity.class);
+
+    @Test
+    public void launchNewActivityTest() {
+        onView(withId(R.id.newTweetFAB)).perform(click());
+
+        onView(allOf(isAssignableFrom(TextView.class), withParent(isAssignableFrom(Toolbar.class)))).
+                check(matches(withText(R.string.title_activity_new_tweet)));
+    }
+}
