c36ed622beca8f6862113e6ec4c06bc315987c1e
Andrea Pivetta <vanpivix@gmail.com>
2016-01-27 00:55:16 +0100

    Ads & IAP removed

--- app/src/main/java/com/andreapivetta/blu/activities/HomeActivity.java
+++ app/src/main/java/com/andreapivetta/blu/activities/HomeActivity.java
@@ -1,28 +1,20 @@
 package com.andreapivetta.blu.activities;
 
-import android.annotation.SuppressLint;
 import android.app.SearchManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.v4.view.MenuItemCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.ProgressBar;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.andreapivetta.blu.R;
 import com.andreapivetta.blu.data.DatabaseManager;
@@ -33,15 +25,8 @@ import com.andreapivetta.blu.services.CheckFollowingService;
 import com.andreapivetta.blu.services.PopulateDatabasesService;
 import com.andreapivetta.blu.services.StreamNotificationService;
 import com.andreapivetta.blu.twitter.TwitterUtils;
-import com.andreapivetta.blu.twitter.UpdateTwitterStatus;
-import com.anjlab.android.iab.v3.BillingProcessor;
-import com.anjlab.android.iab.v3.SkuDetails;
-import com.anjlab.android.iab.v3.TransactionDetails;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import twitter4j.Paging;
@@ -59,8 +44,6 @@ public class HomeActivity extends TimeLineActivity {
     private DataUpdateReceiver dataUpdateReceiver;
     private int mNotificationsCount = 0, newTweetsCount = 0, mMessageCount = 0;
     private ArrayList<Status> upComingTweets = new ArrayList<>();
-
-    private BillingProcessor billingProcessor;
 
     @Override
     @SuppressWarnings("unchecked")
@@ -106,17 +89,6 @@ public class HomeActivity extends TimeLineActivity {
 
             if (newTweetsCount > 0)
                 getSupportActionBar().setTitle(getResources().getQuantityString(R.plurals.new_tweets, newTweetsCount, newTweetsCount));
-        }
-
-        if (!mSharedPreferences.getBoolean(getString(R.string.pref_ads_removed), false)) {
-            AdView mAdView = (AdView) findViewById(R.id.adView);
-            mAdView.setVisibility(View.VISIBLE);
-            AdRequest adRequest = new AdRequest.Builder()
-                    .addTestDevice("BF5C84C7EE32D459CE186190E0A187C7") // Nexus6p emulator
-                    .addTestDevice("D92FD0DC69AECDDD9D41C63DEF1D68C4") // Nexus4
-                    .addTestDevice("E2FC22DC6449FC5B5658CD110B58923A") // LG
-                    .build();
-            mAdView.loadAd(adRequest);
         }
     }
 
@@ -175,9 +147,6 @@ public class HomeActivity extends TimeLineActivity {
 
         if (dataUpdateReceiver != null)
             unregisterReceiver(dataUpdateReceiver);
-
-        if (billingProcessor != null)
-            billingProcessor.release();
     }
 
     @Override
@@ -214,11 +183,7 @@ public class HomeActivity extends TimeLineActivity {
         super.onCreateOptionsMenu(menu);
         menu.clear();
 
-        if (mSharedPreferences.getBoolean(getString(R.string.pref_ads_removed), false))
-            getMenuInflater().inflate(R.menu.menu_home, menu);
-        else
-            getMenuInflater().inflate(R.menu.menu_home_ads, menu);
-
+        getMenuInflater().inflate(R.menu.menu_home, menu);
         MenuItem item = menu.findItem(R.id.action_notifications);
         MenuItemCompat.setActionView(item, R.layout.menu_notification_button);
         View view = MenuItemCompat.getActionView(item);
@@ -281,117 +246,9 @@ public class HomeActivity extends TimeLineActivity {
             onSearchRequested();
         } else if (item.getItemId() == R.id.action_profile) {
             startActivity(new Intent(HomeActivity.this, UserActivity.class));
-        } else if (item.getItemId() == R.id.action_remove_ads) {
-            final AlertDialog.Builder builder = new AlertDialog.Builder(HomeActivity.this);
-            final View dialogView = View.inflate(HomeActivity.this, R.layout.dialog_remove_ads, null);
-
-            final ProgressBar loadingProgressBar = (ProgressBar) dialogView.findViewById(R.id.loadingProgressBar);
-            final RadioGroup radioGroup = (RadioGroup) dialogView.findViewById(R.id.iapRadioGroup);
-
-            ((RadioButton) dialogView.findViewById(R.id.tweet_radioButton)).setText(getString(R.string.tweet));
-            builder.setTitle(getString(R.string.iap_pref_title))
-                    .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            switch (radioGroup.getCheckedRadioButtonId()) {
-                                case R.id.iap_099_radioButton:
-                                    billingProcessor.purchase(HomeActivity.this, "remove_ads_099");
-                                    break;
-                                case R.id.iap_199_radioButton:
-                                    billingProcessor.purchase(HomeActivity.this, "remove_ads_199");
-                                    break;
-                                case R.id.iap_499_radioButton:
-                                    billingProcessor.purchase(HomeActivity.this, "remove_ads_049");
-                                    break;
-                                case R.id.iap_999_radioButton:
-                                    billingProcessor.purchase(HomeActivity.this, "remove_ads_999");
-                                    break;
-                                case R.id.tweet_radioButton:
-                                    new AlertDialog.Builder(HomeActivity.this).setTitle(getString(R.string.pay_with_tweet_title))
-                                            .setMessage(getString(R.string.pay_with_tweet_status))
-                                            .setPositiveButton(getString(R.string.tweet), new DialogInterface.OnClickListener() {
-                                                @SuppressLint("CommitPrefEdits")
-                                                @Override
-                                                public void onClick(DialogInterface dialog, int which) {
-                                                    new UpdateTwitterStatus(HomeActivity.this, twitter, -1l)
-                                                            .execute(getString(R.string.pay_with_tweet_status));
-                                                    mSharedPreferences.edit().putBoolean(getString(R.string.pref_ads_removed), true).commit();
-                                                    Toast.makeText(HomeActivity.this, R.string.completed_purchase, Toast.LENGTH_SHORT).show();
-                                                    finish();
-                                                    startActivity(getIntent());
-                                                }
-                                            }).setNegativeButton(getString(R.string.keep_ads), null)
-                                            .create().show();
-                                    break;
-                            }
-                        }
-                    })
-                    .setNegativeButton(R.string.cancel, null)
-                    .setView(dialogView)
-                    .create().show();
-
-            billingProcessor = new BillingProcessor(HomeActivity.this, null, new BillingProcessor.IBillingHandler() {
-                @Override
-                public void onProductPurchased(String s, TransactionDetails transactionDetails) {
-                    mSharedPreferences.edit().putBoolean(getString(R.string.pref_ads_removed), true).commit();
-                    Toast.makeText(HomeActivity.this, R.string.completed_purchase, Toast.LENGTH_SHORT).show();
-                    finish();
-                    startActivity(getIntent());
-                }
-
-                @Override
-                public void onPurchaseHistoryRestored() {
-
-                }
-
-                @Override
-                public void onBillingError(int i, Throwable throwable) {
-                    Toast.makeText(HomeActivity.this, R.string.failed_purchase, Toast.LENGTH_SHORT).show();
-                }
-
-                @Override
-                public void onBillingInitialized() {
-                    new Handler().postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            if (setUpDialog(dialogView)) {
-                                loadingProgressBar.setVisibility(View.GONE);
-                                radioGroup.setVisibility(View.VISIBLE);
-                            } else {
-                                finish();
-                                startActivity(getIntent());
-                            }
-                        }
-                    }, 2000);
-                }
-            });
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    private boolean setUpDialog(View dialogView) {
-        if (billingProcessor.isPurchased("remove_ads_099") || billingProcessor.isPurchased("remove_ads_199") ||
-                billingProcessor.isPurchased("remove_ads_049") || billingProcessor.isPurchased("remove_ads_999")) {
-            mSharedPreferences.edit().putBoolean(getString(R.string.pref_ads_removed), true).apply();
-            return false;
-        }
-
-        String[] ids = new String[]{"remove_ads_099", "remove_ads_199", "remove_ads_049", "remove_ads_999"};
-        List<SkuDetails> details = billingProcessor.getPurchaseListingDetails(new ArrayList<>(Arrays.asList(ids)));
-        for (SkuDetails detail : details) {
-            if (ids[0].equals(detail.productId)) {
-                ((RadioButton) dialogView.findViewById(R.id.iap_099_radioButton)).setText(detail.priceText);
-            } else if (ids[1].equals(detail.productId)) {
-                ((RadioButton) dialogView.findViewById(R.id.iap_199_radioButton)).setText(detail.priceText);
-            } else if (ids[2].equals(detail.productId)) {
-                ((RadioButton) dialogView.findViewById(R.id.iap_499_radioButton)).setText(detail.priceText);
-            } else {
-                ((RadioButton) dialogView.findViewById(R.id.iap_999_radioButton)).setText(detail.priceText);
-            }
-        }
-
-        return true;
     }
 
     @Override
